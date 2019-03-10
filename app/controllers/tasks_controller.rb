ff9c67aa902b45f655d8f4753f282cd656b21d84
naru.naru <naruzin123@gmail.com>
2019-03-10 12:28:05 +0000

    kadai-tasklist4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,10 +46,10 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常にTask削除されました'
     redirect_to tasks_url
   end
-end
 
  private
 
- def task_params
+  def task_params
     params.require(:task).permit(:content)
- end
+  end
+end
