69412c6d3890ba2a8bd1a976dcd8355590557957
naru.naru <naruzin123@gmail.com>
2019-03-15 02:04:58 +0000

    Bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks=Task.all
+     @tasks = Task.all
   end
 
   def show
+     @task = Task.find(params[:id])
   end
 
   def new
@@ -25,9 +25,11 @@ class TasksController < ApplicationController
   end
 
   def edit
+     @task = Task.find(params[:id])
   end
 
   def update
+     @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -39,6 +41,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常にTask削除されました'
