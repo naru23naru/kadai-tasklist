2011b93b16791776cd3e4111cd75d87d4a3674a2
naru.naru <naruzin123@gmail.com>
2019-03-26 03:57:36 +0000

    log

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -9,7 +10,6 @@ class TasksController < ApplicationController
   end
 
   def show
-     @task = Task.find(params[:id])
   end
 
   def new
@@ -29,12 +29,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-     @task = Task.find(params[:id])
   end
 
   def update
-     @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -45,7 +42,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常にTask削除されました'
@@ -61,5 +57,10 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content,:status)
+  end
+  
+  def correct_user
+    @task = Task.find(params[:id])
+    redirect_to root_url if @task.user != current_user
   end
 end
--- db/migrate/20190322094153_add_user_to_tasks.rb
+++ db/migrate/20190322094153_add_user_to_tasks.rb
@@ -1,5 +1,8 @@
 class AddUserToTasks < ActiveRecord::Migration[5.0]
   def change
+    # add_referenceがやってることは何か
+    # user_idというカラムをintegerで追加している
+    # add_column :tasks, :user_id, :integer
     add_reference :tasks, :user, foreign_key: true
   end
 end
